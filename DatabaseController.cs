1255305f6ba5d73c124bab85066cf20820722869
Milan Jakubec <jakubeclp@gmail.com>
2024-07-03 15:37:32 +0200

    feat: prep DB controller

---
+++ DatabaseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace CodingTracker
+{
+    public class DatabaseController
+    {
+        private string connectionString;
+        private SqliteConnection connection;
+
+        public DatabaseController(string connectionString)
+        {
+            this.connectionString = connectionString;
+            this.connection = new SqliteConnection(connectionString);
+        }
+
+        public void InitializeDatabase()
+        {
+            var createSql = @"CREATE TABLE IF NOT EXISTS SessionsTable ( Id INTEGER PRIMARY KEY, Start TEXT, End TEXT, Duration TEXT) ";
+        }
+
+        public void InsertSession()
+        {
+
+        }
+
+        public void ViewSessions()
+        {
+
+        }
+
+        public void UpdateSession()
+        {
+
+        }
+
+        public void DeleteSession()
+        {
+
+        }
+
+
+    }
+}
