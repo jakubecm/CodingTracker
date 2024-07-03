ac1167c21e454b3dd602915fc9c8e6b845f17c6f
Milan Jakubec <jakubeclp@gmail.com>
2024-07-03 15:32:58 +0200

    feat: model preparation

---
+++ CodingSession.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CodingTracker
+{
+    public class CodingSession
+    {
+        private int SessionId { get; set; }
+        private DateTime StartTime { get; set; }
+        private DateTime EndTime { get; set; }
+        private TimeSpan Duration { get; set; }
+
+        public CodingSession(int sessionId, DateTime startTime, DateTime endTime, TimeSpan duration)
+        {
+            SessionId = sessionId;
+            StartTime = startTime;
+            EndTime = endTime;
+            Duration = duration;
+        }
+
+        private TimeSpan CalculateDuration(DateTime start, DateTime end)
+        {
+            return end - start;
+        }
+    }
+}
