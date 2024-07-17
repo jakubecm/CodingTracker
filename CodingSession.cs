8815e0ab8042054eba476b67d43b9a927d9aa284
Milan Jakubec <jakubeclp@gmail.com>
2024-07-17 20:56:36 +0200

    fix: remove session id as a property from "model"
    - Will autoincrement or generate sessionId in DB

--- CodingSession.cs
+++ CodingSession.cs
@@ -3,14 +3,12 @@ namespace CodingTracker
 {
     public class CodingSession
     {
-        private int SessionId { get; set; }
         private DateTime StartTime { get; set; }
         private DateTime EndTime { get; set; }
         private TimeSpan Duration { get; set; }
 
-        public CodingSession(int sessionId, DateTime startTime, DateTime endTime, TimeSpan duration)
+        public CodingSession(DateTime startTime, DateTime endTime, TimeSpan duration)
         {
-            SessionId = sessionId;
             StartTime = startTime;
             EndTime = endTime;
             Duration = duration;
